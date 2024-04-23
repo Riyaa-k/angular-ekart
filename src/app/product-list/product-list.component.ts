abaa968cf3982bf53909407a447f19610ab8e184
Riyaa-k <anshitakoshta28@gmail.com>
2024-04-23 20:57:14 +0530

    Property binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,12 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  name="John doe"
   product={
     name:"P1",
     price:400,
     colour:"Black",
     discount:8.5,
-    instock:1
+    instock:0,
+    image:"/assets/images/1.png"
   }
   getdiscount(){
     return this.product.price-(this.product.price*this.product.discount/100);
