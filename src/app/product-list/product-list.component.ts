597b8c78dcd919ccad8ff1728ceeb62d5d38f699
Riyaa-k <anshitakoshta28@gmail.com>
2024-04-23 21:16:20 +0530

    event binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  name="John doe"
+  // name="John doe"
+  Addtocart:number=0
   product={
     name:"P1",
     price:400,
@@ -17,6 +18,21 @@ export class ProductListComponent {
   }
   getdiscount(){
     return this.product.price-(this.product.price*this.product.discount/100);
+  }
+  handleinput(event :any){
+    // this.name=event.target.value;
+
+    
+  }
+  increment(){
+    this.Addtocart++;
+  }
+  decrement(){
+    if(this.Addtocart>0){
+
+      this.Addtocart--;
+    }
+
   }
 
 }
